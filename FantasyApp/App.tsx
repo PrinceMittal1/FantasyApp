a1e772b23e2cef0fb9dbb1081e2cec84e9c060b5
PrinceMittal1 <princemittal345@gmail.com>
2024-03-13 15:41:05 +0530

    inital commit

---
+++ FantasyApp/App.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import {View, Text, StyleSheet} from "react-native"
+
+function App(): React.JSX.Element {
+
+  return (
+    <View>
+      <Text>new app for fantsay app</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
